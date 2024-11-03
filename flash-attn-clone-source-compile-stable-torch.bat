aea1865727e4fb23fa32c196795cb313dc6a859d
SingularityMan <carlosquinones007@gmail.com>
2024-11-03 12:24:04 -0500

    .bat file to compile/install flash_attn for Windows. Make sure to carefully edit the .bat file in order to compile with a version compatible with your CUDA/python version.

---
+++ flash-attn-clone-source-compile-stable-torch.bat
@@ -0,0 +1,44 @@
+:: FLASH ATTENTION + STABLE TORCH
+
+
+:: Uninstall existing flash-attn
+pip uninstall flash-attn
+
+
+:: Install torch stable
+pip uninstall torch torchvision torchaudio
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+
+
+:: Install dependencies
+pip install packaging
+pip install wheel
+
+
+:: Install ninja for faster compile
+pip uninstall -y ninja && pip install ninja
+
+
+:: clone flash attention
+git clone https://github.com/Dao-AILab/flash-attention.git
+cd flash-attention
+
+
+:: set the number of threads. 10 jobs uses 60gigs of ram at peak
+set MAX_JOBS=4
+
+
+:: start compile
+:: python setup.py install
+
+
+:: build wheel
+python setup.py bdist_wheel
+
+
+:: pip install some-package.whl
+cd dist
+pip install flash_attn-2.6.3-cp311-cp311-win_amd64.whl
+
+
+pause
