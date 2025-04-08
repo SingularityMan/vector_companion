70736d7665687251f2634c1f7e5f1761d85700a9
SingularityMan <carlosquinones007@gmail.com>
2025-04-08 00:29:07 -0400

    added folder for deep search queries

---
+++ scraped_data/videos/videos.bat
@@ -0,0 +1,11 @@
+# Your video urls will be placed here for download. Previous urls will be removed when a new search initiates.
+https://www.youtube.com/watch?v=y0sF5xhGreA
+https://www.youtube.com/watch?v=bBl5c2DWvRo
+https://www.youtube.com/watch?v=lwviF6tvjvY
+https://www.youtube.com/watch?v=MlDtL2hIj-Q
+https://www.youtube.com/watch?v=5P6JohWZFQY
+https://www.youtube.com/watch?v=co47u19cbds
+https://www.youtube.com/watch?v=js6IrC_6VcM
+https://www.youtube.com/watch?v=QtC3Bo9B0yI
+https://www.youtube.com/watch?v=IDFGOb71AIY
+https://www.youtube.com/watch?v=abuAUjFv88s
